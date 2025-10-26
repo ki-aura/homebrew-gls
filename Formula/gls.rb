cd50f9798a6efeee4eb5ab69078b4db5f71c1722
Matt Guest <mjcguest@gmail.com>
2025-10-26 21:04:13 +0000

    ready for v1.2.0

--- Formula/gls.rb
+++ Formula/gls.rb
@@ -1,8 +1,8 @@
 class Gls < Formula
   desc "Tyler File List"
   homepage "https://github.com/ki-aura/gls"
-  url "https://github.com/ki-aura/gls/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "976f2253ed94b14cc670ec9017a92c657230794ea4e1bd2878c80e604b04baaa"
+  url "https://github.com/ki-aura/gls/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "6da06a78eb7911a53d9b9c5abfd5102f2cfba3aef3a7474b8a2ca5edab491e23"
   license "MIT"
 
   def install
